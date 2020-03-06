2d0b79bcecb9c90798ccdcf137ca6a2f1f0d0df3
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2020-03-06 00:41:41 -0700

    Tightend up animation, working on back to top button

--- js/index.js
+++ js/index.js
@@ -1,52 +1,71 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-/** 1. Intiate wow js
- **************************************************************** **/	
+  /** 1. Intiate wow js
+   **************************************************************** **/
   new WOW().init();
 
-/** 2. Smooth Scrolling
- **************************************************************** **/	
-  let scroll = new SmoothScroll('a[href*="#"]', {offset: 80,speed:2000});
+
+  /** 1. Back to Top" Button
+   **************************************************************** **/
+  let btn = $('#btnTop');
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function (e) {
+    e.preventDefault();
+    $('html,body').animate({ scrollTop: 0 }, 300);
+  });
 
 
-/** 3. Active Link -- not working -- skips experience??
- **************************************************************** **/	
-  $(window).scroll(function() {
-      
-      let scrollLink = $('.scroll-link')
+  /** 2. Smooth Scrolling
+   **************************************************************** **/
+  let scroll = new SmoothScroll('a[href*="#"]', { offset: 80, speed: 900 });
 
-      let scrollbarLocation = $(this).scrollTop();
 
-      scrollLink.each(function() {
-        
-        let sectionOffset = $(this.hash).offset().top - 325;
-        
-        if ( sectionOffset <= scrollbarLocation ) {
-          $(this).parent().addClass('active') ;
-          $(this).parent().siblings().removeClass('active') ;
-        }
-      })
+  /** 3. Active Link 
+   **************************************************************** **/
+  $(window).scroll(function () {
+
+    let scrollLink = $('.scroll-link')
+
+    let scrollbarLocation = $(this).scrollTop();
+
+    scrollLink.each(function () {
+
+      let sectionOffset = $(this.hash).offset().top - 325;
+
+      if (sectionOffset <= scrollbarLocation) {
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+      }
     })
+  })
 
   /** 4. Navbar toggle icon  --> 
- **************************************************************** **/	
-  $('.navbar-nav>li>a').on('click', function(){
+ **************************************************************** **/
+  $('.navbar-nav>li>a').on('click', function () {
     $('.navbar-collapse').collapse('hide');
   });
 
 
 
   /** 5. Modal --> Keeps portfolio modals above the portfolio section. Need to finesse a little. 
- **************************************************************** **/	
-  
-  $("body").on("click",".modalClick",function(){
-    
+ **************************************************************** **/
+
+  $("body").on("click", ".modalClick", function () {
+
     // data-target id that corresponds to the matching modal id
     let modalBtn = $(this.id)
-    
+
     modalBtn.modal("show");
     $("#portfolio").addClass("after_modal_appended");
-  
+
     //removes the padding right and modal-open class from the body tag which bootstrap adds when a modal is shown
     // $('body').removeClass("modal-open")
     //   $('body').css("padding-right","");     
@@ -54,25 +73,25 @@ $(document).ready(function() {
 
 
   // Stops video from playing once modal is closed 
-  $('#sfmModal').on('hidden.bs.modal', function(e) {
+  $('#sfmModal').on('hidden.bs.modal', function (e) {
     $('#sfmModal iframe').attr("src", $('#sfmModal iframe').attr('src'));
   });
 
-//The code below will allows the modal to stay open for design and editing. Just change the modal id. Comment out when finished.
-// $('#fiftyWhips').modal('toggle')
+  //The code below will allows the modal to stay open for design and editing. Just change the modal id. Comment out when finished.
+  // $('#fiftyWhips').modal('toggle')
 
 
-/** 6. Bootstrap Form Validation --> Do we need?
- **************************************************************** **/	
+  /** 6. Bootstrap Form Validation --> Do we need?
+   **************************************************************** **/
 
-  (function() {
+  (function () {
     'use strict';
-    window.addEventListener('load', function() {
+    window.addEventListener('load', function () {
       // Fetch all the forms we want to apply custom Bootstrap validation styles to
       let forms = document.getElementsByClassName('needs-validation');
       // Loop over them and prevent submission
-      let validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
+      let validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener('submit', function (event) {
           if (form.checkValidity() === false) {
             event.preventDefault();
             event.stopPropagation();
